5f089ed07012bda85b0bf9f76da45fec7fd08d42
Austin Douglas <austindouglas@outlook.com>
2025-02-27 19:45:27 -0500

    Revert cave back to marchCubes

--- Editor/TunnelGeneratorEditor.cs
+++ Editor/TunnelGeneratorEditor.cs
@@ -2,14 +2,14 @@ using static UnityEngine.GraphicsBuffer;
 using UnityEditor;
 using UnityEngine;
 
-[CustomEditor(typeof(MarchingCubesGenerator))]
+[CustomEditor(typeof(TunnelGenerator))]
 public class TunnelGeneratorEditor : Editor
 {
     public override void OnInspectorGUI()
     {
         DrawDefaultInspector(); // Draws the default inspector UI
 
-        MarchingCubesGenerator generator = (MarchingCubesGenerator)target;
+        TunnelGenerator generator = (TunnelGenerator)target;
 
         if (GUILayout.Button("Regenerate Cave"))
         {
